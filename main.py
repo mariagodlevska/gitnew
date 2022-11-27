2d0550305abe0b47266e8522eb8cb45ff090510b
Alice <cpython.projects@gmail.com>
2022-11-27 12:49:52 +0300

    add module_stings

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import module_strings
+
 def get_number():
     import random
     return random.randint(1, 100)
 
 def get_hello():
     return 'hello'
+
+
+if __name__ == '__main__':
+    print(module_strings.func())
 
---
+++ module_strings.py
@@ -0,0 +1,2 @@
+def func():
+    return 'tra-la-la'
